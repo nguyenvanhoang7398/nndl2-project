06f0f55af08e20f362435a7f8a44b9559a0139c3
dexterdley <dexterdley94@gmail.com>
2021-03-27 16:15:35 +0800

    Luke

--- examples/ner/model.py
+++ examples/ner/model.py
@@ -24,6 +24,7 @@ class LukeForNamedEntityRecognition(LukeEntityAwareAttentionModel):
 
     def forward(
         self,
+        tokens,
         word_ids,
         word_segment_ids,
         word_attention_mask,
@@ -36,6 +37,7 @@ class LukeForNamedEntityRecognition(LukeEntityAwareAttentionModel):
         labels=None,
     ):
         encoder_outputs = super(LukeForNamedEntityRecognition, self).forward(
+            tokens,
             word_ids,
             word_segment_ids,
             word_attention_mask,
